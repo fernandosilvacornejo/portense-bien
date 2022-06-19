c53517f00e9178cac862ae71ef4348570f4cbdfd
fernandosilvacornejo <fernandosilvacornejo@users.noreply.github.com>
2022-06-18 23:35:08 -0400

    Agregar suma funcional de puntos

--- App.js
+++ App.js
@@ -2,9 +2,6 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useState } from "react";
 import { View, Text, StyleSheet, useWindowDimensions } from 'react-native';
 import { TabView, TabBar, SceneMap } from 'react-native-tab-view';
-// import AppContext from './AppContext';
-
-import TaskItem from './TaskItem';
 import ProfileTab from './ProfileTab';
 
 global.tasks = [
@@ -26,41 +23,16 @@ export default function App() {
     { key: 'bruno', title: 'Bruno' },
     { key: 'dante', title: 'Dante' },
   ]);
-  const [tasksList, updateTasksList] = useState([
-    {'name': 'Tarea 1', 'points': 100},
-    {'name': 'Tarea 2', 'points': 300}
-  ])
-  const [profilePoints, updateProfilePoints] = useState({
-    'bruno': 3600,
-    'dante': 1200
-  })
-
-  const completeTask = (profile, points) => {
-    console.log(profilePoints)
-    const newPoints = global.points
-    newPoints[profile] += points
-    global.points = newPoints
-    updateProfilePoints(newPoints)
-    console.log(profilePoints)
-  }
-
-  // const globalSettings = {
-  //   tasksList: tasksList,
-  //   profilePoints: points,
-  //   completeTask, updateProfilePoints
-  // };
 
   return (
-    // <AppContext.Provider value={globalSettings}>
       <TabView
-        navigationState={{ index, routes, profilePoints: profilePoints }}
+        navigationState={{ index, routes }}
         renderScene={renderScene}
         onIndexChange={setIndex}
         initialLayout={{ width: layout.width }}
         style={{ paddingTop: 40 }}
         renderTabBar={renderTabBar}
       />
-    // </AppContext.Provider>
   );
 }
 
@@ -73,7 +45,6 @@ const renderTabBar = props => (
     activeColor='darkgoldenrod'
   />
 );
-const renderScene = ({ route, profilePoints }) => {
-  console.log(profilePoints)
-  return <ProfileTab name={route.key} points={profilePoints}/>
+const renderScene = ({ route }) => {
+  return <ProfileTab name={route.key} profilePoints={global.points}/>
 }
--- ProfileTab.js
+++ ProfileTab.js
@@ -1,22 +1,31 @@
 import React, { useContext } from 'react';
-import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
+import { Image, ScrollView, StyleSheet, Text, View, TouchableOpacity} from "react-native";
 import { EvilIcons } from '@expo/vector-icons';
 import NumberFormat from 'react-number-format';
 
+export default class ProfileTab extends React.Component {
 
-export default ProfileTab = (props) => {
+  state = {
+      points: this.props.profilePoints[this.props.name],
+  }
 
-    const images = {
+  writeData = (addedPoints) => {
+      this.setState({
+          points: this.state.points + addedPoints
+     })
+  }
+
+  render() {
+    images = {
       'bruno': require('./assets/bruno.png'),
       'dante': require('./assets/dante.png')
     }
-
     return (
       <View style={styles.profileContainer}>
         <View style={{flex: 0.4, width: '100%', alignItems: 'center'}}>
-          <Image style={styles.profilePicture} source={images[props.name]}/>
+          <Image style={styles.profilePicture} source={images[this.props.name]}/>
             <NumberFormat
-                    value={points[props.name]}
+                    value={this.state.points}
                     displayType="text"
                     thousandSeparator="."
                     decimalSeparator=","
@@ -29,13 +38,29 @@ export default ProfileTab = (props) => {
           <ScrollView style={styles.scrollView}>
               {
               global.tasks.map((task, index) => {
+                const points = task['points']
+                const name = task['name']
                 return (
-                  <View key={index} style={styles.taskContainer}>
-                    <TaskItem
-                        points={task['points']}
-                        name={task['name']}
-                        profile={props.name}
-                    />
+                  <View key={index} style={styles.mainTaskContainer}>
+                    <View style={styles.container}>
+                        <View style={styles.taskContainer}>
+                            <Text style={styles.task}>{name}</Text>
+                        </View>
+                        <View style={styles.pointsContainer}>
+                              <NumberFormat
+                                value={points}
+                                displayType="text"
+                                thousandSeparator="."
+                                decimalSeparator=","
+                                suffix=" pts"
+                                renderText=
+                                  {(value) => <Text style={styles.taskPoints}>{value}</Text>}
+                              />
+                            <TouchableOpacity onPress={() => this.writeData(points)}>
+                                <EvilIcons style={styles.complete} name="plus" size={35} />
+                            </TouchableOpacity>
+                        </View>
+                    </View>
                   </View>
                 );
               })
@@ -44,28 +69,66 @@ export default ProfileTab = (props) => {
         </View>
       </View>
     );
+  }
 }
-
 const styles = StyleSheet.create({
-  profileContainer: {
-    flex: 1,
-    paddingTop: 20,
-    backgroundColor: 'whitesmoke',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '100%'
-  },
-  profilePicture: {
-      width: 200,
-      height: 200,
-      borderRadius: 200 / 2
-  },
-  profilePoints: {
-    fontWeight: 'bold',
-    fontSize: 30,
-    padding: 10
-  },
-  taskContainer: {
-      flexDirection: 'row',
-  },
-});
+    profileContainer: {
+      flex: 1,
+      paddingTop: 20,
+      backgroundColor: 'whitesmoke',
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: '100%'
+    },
+    profilePicture: {
+        width: 200,
+        height: 200,
+        borderRadius: 200 / 2
+    },
+    profilePoints: {
+      fontWeight: 'bold',
+      fontSize: 30,
+      padding: 10
+    },
+    mainTaskContainer: {
+        flexDirection: 'row',
+    },
+    container: {
+        flexDirection: 'row',
+        backgroundColor: 'white',
+        marginHorizontal: 20,
+        borderRadius: 12,
+        borderWidth: 3,
+        borderColor: "goldenrod",
+        width: '90%',
+        marginBottom: 20,
+        minHeight: 50
+
+    },
+    taskContainer: {
+      margin: 10,
+      marginLeft: 20,
+      flex: 5,
+      justifyContent: 'center'
+    },
+    task: {
+      color: 'darkgoldenrod',
+      fontSize: 16,
+    },
+    pointsContainer: {
+        flex: 2,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'goldenrod'
+    },
+    taskPoints: {
+        color: 'lightgoldenrodyellow',
+        fontSize: 15,
+        padding: 5
+    },
+    complete: {
+      color: "lightgoldenrodyellow",
+      paddingBottom: 10,
+
+    },
+  });
